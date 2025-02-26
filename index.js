ac1989e30735e56be84627c0bd77b90a64bdb39d
SAM's pc <Waliur Rafiq Sami>
2025-02-26 20:54:57 +0600

    change url

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://artfusion-f9745.web.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
